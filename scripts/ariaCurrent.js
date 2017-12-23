4fafe0b5c0c853683fe3509e781cd0ce46c2a761
Noelia Ruiz Martínez <nrm1977@gmail.com>
2017-12-23 11:14:48 +0100

    Test why aria-current is not read: remove accesskey

--- scripts/ariaCurrent.js
+++ scripts/ariaCurrent.js
@@ -3,6 +3,7 @@ function setAriaCurrent() {
 	if title== "Comunidad hispanohablante de NVDA" {
 		var id = "inicio"
 	}
+	document.getElementById(id).removeAttribute("accesskey");
 	document.getElementById(id).setAttribute("aria-current", "page");
 }
 
