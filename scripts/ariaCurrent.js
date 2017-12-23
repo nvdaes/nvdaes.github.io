d99c7667b4893562801980c98338da9548a2aa42
Noelia Ruiz Martínez <nrm1977@gmail.com>
2017-12-23 10:04:02 +0100

    Test: Try to set aria-current in nav

---
+++ scripts/ariaCurrent.js
@@ -0,0 +1,8 @@
+function setAriaCurrent() {
+	var title = document.title;
+	if title === "Comunidad hispanohablante de NVDA" {
+		document.getElementById("inicio").setAttribute("aria-current", "page");
+	} else if title.includes("- Ayuda y descarga") {
+		document.getElementById("ayuda".setAttribute("aria-current", "page");
+	}
+}
