669e1a0bd85db8a14e0a64392f1151746e81aa34
Noelia Ruiz Martínez <nrm1977@gmail.com>
2017-12-23 11:52:14 +0100

    Test without conditions

--- scripts/ariaCurrent.js
+++ scripts/ariaCurrent.js
@@ -1,8 +1,5 @@
 function setAriaCurrent() {
-	var title = document.title
-	if title.includes("Ayuda y descarga") {
-		document.getElementById("ayuda".setAttribute("aria-current", "page");
-	}
+	document.getElementById("ayuda".setAttribute("aria-current", "page");
 }
 
 setAriaCurrent();
