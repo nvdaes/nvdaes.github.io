93194d63fc7c9c031f009c93007ba943d80b829c
Noelia Ruiz Martínez <nrm1977@gmail.com>
2017-12-23 11:47:38 +0100

    Javascript: Test with a simplified expression

--- scripts/ariaCurrent.js
+++ scripts/ariaCurrent.js
@@ -1,9 +1,7 @@
 function setAriaCurrent() {
-	var title = document.title.trim();
-	if title === "Comunidad hispanohablante de NVDA") {
-		document.getElementById("inicio".setAttribute("aria-current", "page");
-	} else if title.includes("- Ayuda y descarga") {
-		document.getElementById("ayuda").setAttribute("aria-current", "page")
+	var title = document.title
+	if title.includes("Ayuda y descarga") {
+		document.getElementById("ayuda".setAttribute("aria-current", "page");
 	}
 }
 
