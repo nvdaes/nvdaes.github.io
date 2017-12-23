5609893c47d06814bb86489ee64dcd70db7256b9
Noelia Ruiz Martínez <nrm1977@gmail.com>
2017-12-23 10:52:23 +0100

    Set aria-current for first link of nav

--- scripts/ariaCurrent.js
+++ scripts/ariaCurrent.js
@@ -1,6 +1,9 @@
 function setAriaCurrent() {
-	var id = document.getElementById("inicio");
-	id.textContent = document.title
+	var title = document.title;
+	if title== "Comunidad hispanohablante de NVDA" {
+		var id = "inicio"
+	}
+	document.getElementById(id).setAttribute("aria-current", "page");
 }
 
 setAriaCurrent();
