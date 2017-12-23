79017cabe649f66684b4bc2e3b475246e0712693
Noelia Ruiz Martínez <nrm1977@gmail.com>
2017-12-23 10:26:19 +0100

    Test if scripts work

--- scripts/ariaCurrent.js
+++ scripts/ariaCurrent.js
@@ -1,10 +1,6 @@
 function setAriaCurrent() {
-	var title = document.title;
-	if title === "Comunidad hispanohablante de NVDA" {
-		document.getElementById("inicio").setAttribute("aria-current", "page");
-	} else if title.includes("- Ayuda y descarga") {
-		document.getElementById("ayuda".setAttribute("aria-current", "page");
-	}
+	var id = document.getElementById("inicio");
+	id.removeAttribute("accesskey");
 }
 
 setAriaCurrent();
