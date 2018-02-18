18372c4e595d5cf4466634126e4ea3cb04fd4189
Noelia Ruiz Martínez <nrm1977@gmail.com>
2018-02-18 12:07:19 +0100

    Test to include last update with JS

---
+++ scripts/footer.js
@@ -0,0 +1,4 @@
+var footer = document.getElementById("footer");
+var div = document.createElement("DIV");
+div.innerHTML = "<p>ültima actualización: 18 de febrero de 2018</p>"
+footer.appendChild(div);
