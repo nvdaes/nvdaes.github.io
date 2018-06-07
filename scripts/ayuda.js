cdc9b3686e18d0245055fdc4120601e5f0682955
Noelia Ruiz Martínez <nrm1977@gmail.com>
2018-06-08 01:52:22 +0200

    More tests with scripts

--- scripts/ayuda.js
+++ scripts/ayuda.js
@@ -6,13 +6,10 @@ function showLastVersion() {
 		if (this.readyState == 4 && this.status == 200) {
 			var p = document.createElement("p");
 			var docTitle = this.responseXML.title;
-			var version = docTitle.substring(docTitle.search("NVDA"));
-			p.textContent = "Última versión: " + version;
-			document.querySelector(".entry").insertBefore(p);
 		}
 	};
 	xhttp.open("GET", "userGuide.html", true);
 	xhttp.send();
-};
+}
 
 showLastVersion();
