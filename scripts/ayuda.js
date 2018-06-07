54374d7c45be47810aa65d9d401da4bc6eb8e0cd
Noelia Ruiz Martínez <nrm1977@gmail.com>
2018-06-08 01:40:25 +0200

    Test to add last version of NVDA

--- scripts/ayuda.js
+++ scripts/ayuda.js
@@ -1,1 +1,16 @@
 document.getElementById("ayuda").setAttribute("aria-current", "page");
+
+function showLastVersion() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var p = document.createElement("p");
+			var docTitle = this.responseXML.title;
+			var version = docTitle.substring(docTitle.search("NVDA"));
+			p.textContent = "Última versión: " + version;
+			document.querySelector(".entry").insertBefore(p);
+		}
+	};
+	xhttp.open("GET", "userGuide.html", true);
+	xhttp.send();
+	
