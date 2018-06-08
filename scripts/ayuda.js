b9b938113faca5ee1a5a1364384d5d2551d05621
Noelia Ruiz Martínez <nrm1977@gmail.com>
2018-06-08 21:35:00 +0200

    Test with real example of AJAX

--- scripts/ayuda.js
+++ scripts/ayuda.js
@@ -2,21 +2,26 @@ document.getElementById("ayuda").setAttribute("aria-current", "page");
 
 var aside = document.getElementById("complementario");
 var button = document.createElement("BUTTON");
-var text = document.createTextNode("Prueba");
+var text = document.createTextNode("Información sobre placeMarkers");
 button.appendChild(text);
 aside.appendChild(button);
 var div = document.createElement("DIV");
 
-function prueba() {
+function showAddonInfo(addon) {
 		var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			// document.getElementById("demo").innerHTML = this.responseText;
-			alert("hecho");
+			var div.createElement("DIV");
+			var json = JSON.parse(xhttp.responseText);
+			div.innerHTML = json.name;
 		}
 	};
-	xhttp.open("GET", "https://api.github.com/repos/nvdaes/placeMarkers/releases/latest", true);
+	xhttp.open("GET", "https://api.github.com/repos/nvdaes/" + addon + "/releases/latest", true);
 	xhttp.send();
 }
 
-button.addEventListener("click", prueba, true);
+function showPlaceMarkersInfo() {
+	showAddonInfo("placeMarkers");
+}
+
+button.addEventListener("click", showPlaceMarkersInfo, true);
