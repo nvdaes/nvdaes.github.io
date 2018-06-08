969fd7742fd020d2cfb256fd2c07a4e3aca2b39c
Noelia Ruiz Martínez <nrm1977@gmail.com>
2018-06-08 02:02:18 +0200

    Update script

--- scripts/ayuda.js
+++ scripts/ayuda.js
@@ -4,8 +4,13 @@ function showLastVersion() {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			var p = document.createElement("p");
+			var p = document.createElement("P");
 			var docTitle = this.responseXML.title;
+			var versionPos =docTitle.search("NVDA");
+			var version = docTitle.substring(versionPos);
+			t = document.createTextNode(version);
+			p.appendChild(t);
+			document.querySelector("div.entry").insertBefore(p);
 		}
 	};
 	xhttp.open("GET", "userGuide.html", true);
