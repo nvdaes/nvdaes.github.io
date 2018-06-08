c0bce64aa0894030f1cd332517f0bec02ddeb547
Noelia Ruiz Martínez <nrm1977@gmail.com>
2018-06-08 14:54:28 +0200

    First test with AJAX

--- scripts/ayuda.js
+++ scripts/ayuda.js
@@ -1,12 +1,21 @@
 document.getElementById("ayuda").setAttribute("aria-current", "page");
-
-function prueba() {
-	alert("prueba");
-}
-
 var aside = document.getElementById("complementario");
 var button = document.createElement("BUTTON");
 var text = document.createTextNode("Prueba");
 button.appendChild(text);
 aside.appendChild(button);
 button.addEventListener("click", prueba, true);
+
+function prueba() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var div = document.createElement("DIV");
+			div.innerHTML(this.responseText);
+			aside.appendChild(div);
+		}
+	};
+	xhttp.open("GET", "https://api.github.com/repos/nvdaes/repos/placeMarkers/releases/latest", true);
+	xhttp.send();
+}
+
