ff01e510876e09e997d971941dec2b5bcbf88824
Noelia Ruiz Martínez <nrm1977@gmail.com>
2018-06-08 21:45:18 +0200

    Update script

--- scripts/ayuda.js
+++ scripts/ayuda.js
@@ -1,6 +1,7 @@
 document.getElementById("ayuda").setAttribute("aria-current", "page");
 
 var aside = document.getElementById("complementario");
+var div = document.createElement("DIV");
 var button = document.createElement("BUTTON");
 var text = document.createTextNode("Información sobre placeMarkers");
 button.appendChild(text);
@@ -10,9 +11,10 @@ function showAddonInfo(addon) {
 		var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			var div = document.createElement("DIV");
 			var json = JSON.parse(xhttp.responseText);
-			div.innerHTML = json.name;
+			var name = json.name;
+			var text = document.createTextNode(addon + name);
+			div.appendChild(text);
 		}
 	};
 	xhttp.open("GET", "https://api.github.com/repos/nvdaes/" + addon + "/releases/latest", true);
