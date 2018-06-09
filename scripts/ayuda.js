0ffdf20112a25b117d5707108b76497db8d58cf1
Noelia Ruiz Martínez <nrm1977@gmail.com>
2018-06-09 09:28:26 +0200

    Update script

--- scripts/ayuda.js
+++ scripts/ayuda.js
@@ -1,7 +1,10 @@
 document.getElementById("ayuda").setAttribute("aria-current", "page");
 
 var aside = document.getElementById("complementario");
-
+var button = document.createElement("BUTTON");
+button.setAttribute("id", "button");
+var buttonText = document.createTextNode("Información sobre complementos de nvdaes");
+button.appendChild(buttonText);
 
 addons = [
 	"clipContentsDesigner",
@@ -13,21 +16,25 @@ addons = [
 ]
 
 var addonsLength = addons.length;
-for (var i = 0; i < addonsLength; i++) {
-	var details = document.createElement("DETAILS");
-	var summary = document.createElement("SUMMARY");
-	details.setAttribute("id", addons[i]);
-	var t = document.createTextNode(addons[i]);
-	summary.appendChild(t);
-	details.appendChild(summary);
-	aside.appendChild(details);
-}
 
 $(document).ready(function () {
-	$("details").click(function () {
-		$.getJSON("https://api.github.com/repos/nvdaes/" + "placeMarkers" + "/releases/latest", function(json) {
-			var name = json.name;
-			this.innerHTML("<p>"+name+"</p>");
-		});
+	$("#addonsButton").click(function () {
+		for (var i = 0; i < addonsLength; i++) {
+			var details = document.createElement("DETAILS");
+			var summary = document.createElement("SUMMARY");
+			var t = document.createTextNode(addons[i]);
+			summary.appendChild(t);
+			details.appendChild(summary);
+			$.getJSON("https://api.github.com/repos/nvdaes/" + addons[i] + "/releases/latest", function(json) {
+				var name = json.name;
+				var ul = document.createElement("UL");
+				var li = document.createElement("LI");
+				var t = document.createTextNode(name);
+				li.appendChild(t);
+				ul.appendChild(li);")
+				details.appendChild(ul);
+			});
+			aside.appendChild(details);
+		}
 	});
 });
