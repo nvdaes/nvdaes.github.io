1ab48db84b3f02fbc09f403844dbdc5f25681908
Noelia Ruiz Martínez <nrm1977@gmail.com>
2018-06-09 03:20:37 +0200

    Test with script

--- scripts/ayuda.js
+++ scripts/ayuda.js
@@ -25,10 +25,9 @@ for (var i = 0; i < addonsLength; i++) {
 
 $(document).ready(function () {
 	$("details").click(function () {
-		$.getJSON("https://api.github.com/repos/nvdaes/" + this.id + "/releases/latest", function(json) {
+		$.getJSON("https://api.github.com/repos/nvdaes/" + "placeMarkers" + "/releases/latest", function(json) {
 			var name = json.name;
 			this.text(name);
-			alert(this.id);
 		});
 	});
 });
