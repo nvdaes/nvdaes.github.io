3603450f7b9c871dde326ef363f31551c887453f
Noelia Ruiz Martínez <nrm1977@gmail.com>
2018-06-09 02:16:39 +0200

    Using itaration for many add-ons

--- scripts/ayuda.js
+++ scripts/ayuda.js
@@ -2,13 +2,28 @@ document.getElementById("ayuda").setAttribute("aria-current", "page");
 
 var aside = document.getElementById("complementario");
 var details = document.createElement("DETAILS");
-details.setAttribute("id", "details");
-details.setAttribute("summary", "información sobre placeMarkers");
-aside.appendChild(details);
+var summary = document.createElement("SUMMARY");
+
+addons = [
+	"clipContentsDesigner",
+	"emoticons",
+	"eMule",
+	"placeMarkers",
+	"readFeeds",
+	"reportSymbols",
+]
+
+for (addon in addons) {
+	details.setAttribute("id", addon);
+	var t = document.createTextNode(addon);
+	summary.appendChild(t);
+	details.appendChild(summary);
+	aside.appendChild(details);
+}
 
 $(document).ready(function () {
-	$("#details").click(function () {
-		$.getJSON("https://api.github.com/repos/nvdaes/placeMarkers", function(json) {
+	$("details").click(function () {
+		$.getJSON("https://api.github.com/repos/nvdaes/" + $(this).attr("id") + "releases/latest", function(json) {
 			var name = json.name;
 			$("#details").text(name);
 		});
