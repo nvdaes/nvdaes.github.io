4e3cede257f83c728c385a263d09244c53fa5f8e
Noelia Ruiz Martínez <nrm1977@gmail.com>
2018-06-09 03:30:14 +0200

    Update script, now working with alerts

--- scripts/ayuda.js
+++ scripts/ayuda.js
@@ -27,7 +27,7 @@ $(document).ready(function () {
 	$("details").click(function () {
 		$.getJSON("https://api.github.com/repos/nvdaes/" + "placeMarkers" + "/releases/latest", function(json) {
 			var name = json.name;
-			alert(name);
+			this.text = name
 		});
 	});
 });
