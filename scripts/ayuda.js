e760c3e831c46d68d585e7c5f0ddbfead23632a8
Noelia Ruiz Martínez <nrm1977@gmail.com>
2018-06-09 21:02:01 +0200

    Remove submit from script for debugging

--- scripts/ayuda.js
+++ scripts/ayuda.js
@@ -13,14 +13,16 @@ input.setAttribute("type", "text");
 input.setAttribute("id", "repo");
 input.setAttribute("name", "repo");
 label.appendChild(input);
+/*
 var submit = document.createElement("INPUT");
 submit.setAttribute("id", "submit");
 submit.setAttribute("type", "button");
 var t = document.createTextNode("Consultar descargas de última versión");
 submit.appendChild(t);
+*/
 p.appendChild(label);
 form.appendChild(p);
-form.appendChild(submit);
+//form.appendChild(submit);
 aside.appendChild(form);
 
 /*
