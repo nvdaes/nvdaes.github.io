e96c31424bf16afd9afbae2bf17213682f666127
Noelia Ruiz Martínez <nrm1977@gmail.com>
2018-06-10 07:50:56 +0200

    Add option for specifying the version parameter

--- scripts/ayuda.js
+++ scripts/ayuda.js
@@ -7,32 +7,56 @@ h.appendChild(t);
 aside.appendChild(h);
 var form = document.createElement("FORM");
 form.setAttribute("role", "form");
+// First form field
 var p = document.createElement("P");
 var label = document.createElement("LABEL");
 label.setAttribute("for", "repo");
-t = document.createTextNode("cuenta/repositorio de GitHub");
+t = document.createTextNode("cuenta/repositorio de GitHub (requerido)");
 label.appendChild(t);
-var input = document.createElement("INPUT");
-input.setAttribute("type", "text");
-input.setAttribute("id", "repo");
-input.setAttribute("name", "repo");
-input.setAttribute("placeholder", "Ejemplo: nvdaes/emoticons");
-label.appendChild(input);
+var inputRepo = document.createElement("INPUT");
+inputRepo.setAttribute("type", "text");
+inputRepo.setAttribute("id", "repo");
+inputRepo.setAttribute("name", "repo");
+inputRepo.setAttribute("placeholder", "Ejemplo: nvdaes/emoticons");
+label.appendChild(inputRepo);
+p.appendChild(label);
+form.appendChild(p);
+// Second form field
+var p = document.createElement("P");
+var label = document.createElement("LABEL");
+label.setAttribute("for", "version");
+t = document.createTextNode("Versión (opcional). Por defecto se usará la última estable:");
+label.appendChild(t);
+var inputVersion = document.createElement("INPUT");
+inputVersion.setAttribute("type", "text");
+inputVersion.setAttribute("id", "version");
+inputVersion.setAttribute("name", "version");
+inputVersion.setAttribute("placeholder", "Ejemplo: 7.0-dev");
+label.appendChild(inputVersion);
+p.appendChild(label);
+form.appendChild(p);
+
+var p = document.createElement("P");
 var submit = document.createElement("INPUT");
 submit.setAttribute("id", "submit");
 submit.setAttribute("type", "button");
-submit.setAttribute("value", "Consultar descargas de última versión");
-p.appendChild(label);
+submit.setAttribute("value", "Consultar número de descargas");
+p.appendChild(submit);
 form.appendChild(p);
-form.appendChild(submit);
 aside.appendChild(form);
 
 $(document).ready(function () {
 	$("#submit").click(function () {
-		$.getJSON("https://api.github.com/repos/" + input.value + "/releases/latest", function(json) {
+		var version;
+		if ($.trim(inputVersion.value).length === 0) {
+			version = "latest";
+		} else {
+			version = inputVersion.value;
+		}
+		$.getJSON("https://api.github.com/repos/" + inputRepo.value + "/releases/" + version, function(json) {
 			var assetName = json.assets[0].name;
 			var downloadCount = json.assets[0].download_count;
-			alert(assetName + " " + downloadCount);
+			alert(assetName + " - " + downloadCount);
 		});
 	});
 });
