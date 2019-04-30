619fb604f480551e6f3883406daff2c576f24ef7
Noelia Ruiz Martínez <nrm1977@gmail.com>
2019-05-01 01:33:41 +0200

    Try to set end time for audio

--- scripts/chris.js
+++ scripts/chris.js
@@ -1,12 +1,17 @@
 var audio = document.getElementById("chris");
-var div = document.getElementById("audioContainer");
-var button = document.createElement("BUTTON");
-button.setAttribute("type", "button");
-button.setAttribute("onclick", "playChris()");
-button.innerText = "Escuchar a Chris";
-div.insertBefore(button, audio);
+audio.addEventListener("canplaythrough", "addChrisButton()");
+
+function addChrisButton() {
+	var div = document.getElementById("audioContainer");
+	var button = document.createElement("BUTTON");
+	button.setAttribute("type", "button");
+	button.setAttribute("onclick", "playChris()");
+	button.innerText = "Escuchar a Chris";
+	div.insertBefore(button, audio);
+}
 
 function playChris() {
 	audio.currentTime = 952;
 	audio.play();
+	setTimeout(audio.pause(), 331000);
 }
