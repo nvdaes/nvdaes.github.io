ed35a037926235bba93234f85b725f2a8ffdc0a4
Noelia Ruiz Martínez <nrm1977@gmail.com>
2019-05-01 01:08:02 +0200

    Put () in function

--- scripts/chris.js
+++ scripts/chris.js
@@ -2,7 +2,7 @@ var audio = document.getElementById("chris");
 var div = document.getElementById("audioContainer");
 var button = document.createElement("BUTTON");
 button.setAttribute("type", "button");
-button.setAttribute("onclick", "playChris");
+button.setAttribute("onclick", "playChris()");
 button.innerText = "Escuchar a Chris";
 div.insertBefore(button, audio);
 
