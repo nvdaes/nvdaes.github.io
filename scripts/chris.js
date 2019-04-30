2ad74f1dae27cfea4a3dc9229d5cdcbef8520aa3
Noelia Ruiz Martínez <nrm1977@gmail.com>
2019-05-01 00:14:16 +0200

    Testing if audio is recognized

--- scripts/chris.js
+++ scripts/chris.js
@@ -1,7 +1,2 @@
 var audio = document.getElementById("chris");
-audio.ontimeupdate = function() {
-	if (audio.currentTime == 955) {
-		alert("audio");
-		// audio.currentTime = 952;
-	}
-}
+audio.removeAttribute("controls");
