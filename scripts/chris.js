97fa03ff3bf49d09cbd774fc9846b3298d11b2f2
Noelia Ruiz Martínez <nrm1977@gmail.com>
2019-05-01 03:00:55 +0200

    Use interval in Chris button

--- scripts/chris.js
+++ scripts/chris.js
@@ -1,20 +1,28 @@
 var audio = document.getElementById("chris");
 
 function pauseChris() {
-	audio.pause();
+	if (audio.currentTime >= 331000) {
+		audio.pause();
+	}
 }
 
 function playChris() {
 	audio.currentTime = 952;
 	audio.play();
-	setTimeout(pauseChris, 331000);
+	var interval = setInterval(pauseChris, 1000);
+}
+
+function clearInterval() {
+	clearInterval(interval)
 }
 
 function addChrisButton() {
 	var div = document.getElementById("audioContainer");
 	var button = document.createElement("BUTTON");
 	button.setAttribute("type", "button");
-	button.setAttribute("onclick", "playChris()");
+	button.addEventListener("click", playChris);
+	audio.addEventListener("pause", clearInterval);
+	audio.addEventListener("seeking", clearInterval);
 	button.innerText = "Escuchar a Chris";
 	div.insertBefore(button, audio);
 }
