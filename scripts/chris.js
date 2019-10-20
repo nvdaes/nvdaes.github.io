43ed87e7f798cbd6ef0e4fda6051a2b201fcaaea
Noelia Ruiz Martínez <nrm1977@gmail.com>
2019-10-20 19:57:29 +0200

    Improve script

--- scripts/chris.js
+++ scripts/chris.js
@@ -1,16 +1,18 @@
 var audio = document.getElementById("chris");
+var interval;
 
 function pauseChris() {
 	if (952 > audio.currentTime || audio.currentTime >= 1283) {
 		audio.pause();
 		audio.currentTime = 952;
+		clearInterval(interval)
 	}
 }
 
 function playChris() {
 	audio.currentTime = 952;
 	audio.play();
-	var interval = setInterval(pauseChris, 1000);
+	interval = setInterval(pauseChris, 1000);
 }
 
 function addChrisButton() {
