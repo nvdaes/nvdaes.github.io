1f00793857b47895033f89868dfeedc6c99cd32a
Noelia Ruiz Martínez <nrm1977@gmail.com>
2020-07-12 22:23:52 +0200

    Try to add script to copy

---
+++ scripts/opds.js
@@ -0,0 +1,19 @@
+function copy() {
+	let opds = document.getElementById("opds");
+	opds.select()
+	opds.setSelectionRange(0, 99999); /*For mobile devices*/
+	document.execCommand("copy");
+	alert("Enlace copiado: " + opds.value);
+}
+
+function addCopyButton() {
+	let paragraph = document.getElementById("p");
+	let button = document.createElement("BUTTON");
+	button.setAttribute("type", "button");
+	button.addEventListener("click", copy);
+	button.innerText = "Copiar enlace OPDS";
+	paragraph.appendChild(button);
+}
+
+
+addCopyButton();
