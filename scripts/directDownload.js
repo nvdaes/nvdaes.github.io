d3b1702082d5e40d0b257989b26196e1b7cd832e
Noelia Ruiz Martínez <nrm1977@gmail.com>
2021-12-31 08:35:59 +0100

    Use inerText for link

--- scripts/directDownload.js
+++ scripts/directDownload.js
@@ -2,12 +2,14 @@ const createNvdaDownloadLink = async () => {
 	try {
 		const latestRelease = await fetch('https://api.github.com/nvaccess/nvda/releases/latest').json();
 		const versionName = latestRelease.name.substring(9);
-		const prefix = 'http://www.nvaccess.org/download/nvda/releases/';
+		const prefix = 'http://www.nvaccess.org/download/nvda/releases/';`
 		const subfix = '.exe';
 		const url = `{prefix}{versionName}nvda_{versionName}{subfix}`;
 		const link = document.createElement("A");
 		link.accesskey = "0";
+		link.title = "Descarga NVDA desde el servidor de NV Access";
 		link.href = url;
+		link.innerText = `Descarga directa de NVDA {versionName}`;
 		document.getElementById("directDownload").appendChild(link);
 	} catch(err) {
 		console.log(err);
