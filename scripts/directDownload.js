d77c587553b699da92548e5d7757dd33bdd6e108
Noelia Ruiz Martínez <nrm1977@gmail.com>
2021-12-31 09:34:51 +0100

    Test

--- scripts/directDownload.js
+++ scripts/directDownload.js
@@ -1,11 +1,10 @@
 const getDownloadUrl = async () => {
 	try {
 		const release = await fetch('https://api.github.com/nvaccess/nvda/releases/latest').json();
-		return release.name;
+		await document.getElementById("directDownload").innerText = `${release.name}`;
 	} catch(err) {
 		console.log(err);
 	}
 }
 
-const releaseName = getDownloadUrl();
-document.getElementById("directDownload").innerText = `${releaseName}`;
+
