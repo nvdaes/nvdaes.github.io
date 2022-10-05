0db4b7e30a1c324c9090da1eacd6a596d9ff7eec
Noelia Ruiz Martínez <nrm1977@gmail.com>
2022-10-05 13:31:47 +0200

    Add script to copy download link

---
+++ scripts/download.js
@@ -0,0 +1,17 @@
+const downloadParagraph = document.getElementById("downloadParagraph");
+const downloadLink = document.getElementById("directDownload");
+const button = document.createElement("button");
+button.textContent("Copiar enlace de descarga");
+
+button.addEventListener("click", copyUrl);
+
+const copyUrl = () => {
+	try {
+		navigator.clipboard.writeText(downloadLink.getAttribute("href"));
+		alert("Enlace copiado");
+	} catch (err) {
+		alert("No se ha podido copiar el enlace");
+	}
+}
+
+downloadParagraph.appendChild(button);
