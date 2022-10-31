8bae2830ece600d75b6c9985935203e61d51dc6b
Noelia Ruiz Martínez <nrm1977@gmail.com>
2022-10-31 10:15:48 +0100

    Fix link

--- scripts/directLink.js
+++ scripts/directLink.js
@@ -5,7 +5,7 @@ const lastRelease = async () => {
   const response =await fetch(lastReleaseUrl);
   const json = await response.json();
   const releaseName = await json.name;
-  const version = await releaseName.substr(9);
+  const version = await releaseName.substr(8);
   const directLink = await document.createElement('a');
   await directLink.setAttribute('ref', `https://www.nvaccess.org/download/nvda/releases/${version}/nvda_${version}.exe`);
   await directLink.setAttribute('accesskey', '0');
