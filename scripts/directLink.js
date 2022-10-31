c22b1b91eaf824e8ffadfb02950e36b7c8970ea7
Noelia Ruiz Martínez <nrm1977@gmail.com>
2022-10-31 10:25:46 +0100

    Fix href

--- scripts/directLink.js
+++ scripts/directLink.js
@@ -7,7 +7,7 @@ const lastRelease = async () => {
   const releaseName = await json.name;
   const version = await releaseName.substr(8);
   const directLink = await document.createElement('a');
-  await directLink.setAttribute('ref', `https://www.nvaccess.org/download/nvda/releases/${version}/nvda_${version}.exe`);
+  await directLink.setAttribute('href', `https://www.nvaccess.org/download/nvda/releases/${version}/nvda_${version}.exe`);
   await directLink.setAttribute('accesskey', '0');
   directLink.innerText = `Descargar NVDA ${version}`;
   await directDownload.appendChild(directLink);
