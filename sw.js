ee3aa4cb302a1bb6abaf33e6bb64ff5feae092c8
Noelia Ruiz Martínez <nrm1977@gmail.com>
2022-12-28 14:30:53 +0100

    Add offline.html

--- sw.js
+++ sw.js
@@ -2,35 +2,8 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 workbox.loadModule('workbox-strategies');
+workbox.loadModule('workbox-routing');
+workbox.loadModule('workbox-recipes');
 
-const imageRoute = new Route(({ request }) => {
-  return request.destination === 'image'
-}, new workbox.strategies.StaleWhileRevalidate({
-  cacheName: 'images'
-}));
-
-// Handle scripts:
-const scriptsRoute = new Route(({ request }) => {
-  return request.destination === 'script';
-}, new workbox.strategies.NetworkFirst({
-  cacheName: 'scripts'
-}));
-
-// Handle styles:
-const stylesRoute = new Route(({ request }) => {
-  return request.destination === 'style';
-}, new workbox.strategies.CacheFirst({
-  cacheName: 'styles'
-}));
-// Handle pages:
-const pagesRoute = new Route(({ request }) => {
-  return request.destination === 'page';
-}, new workbox.strategies.NetworkFirst({
-  cacheName: 'pages'
-}));
-
-// Register routes
-registerRoute(imageRoute);
-registerRoute(scriptsRoute);
-registerRoute(stylesRoute);
-registerRoute(pagesRoute);
+setDefaultHandler(new workbox.strategies.NetworkOnly());
+workbox.recipes.offlineFallback();
