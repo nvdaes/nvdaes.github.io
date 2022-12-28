eb280eb309581d316dda2fe125c06d6e5d014411
Noelia Ruiz Martínez <nrm1977@gmail.com>
2022-12-28 17:00:53 +0100

    Add pageCache workbox recipe

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
 importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
-workbox.routing.setDefaultHandler(
-  new workbox.strategies.NetworkOnly()
-);
 
+workbox.recipes.pageCache();
 workbox.recipes.offlineFallback({
   pageFallback: '/offline.html'
 });
